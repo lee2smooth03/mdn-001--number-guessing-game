f0ac186bf917d7747dd7d4209e7138163ae31429
Arthur L. Rozier IV <a.rozier4@gmail.com>
2021-03-21 20:40:22 -0400

    html, css, and js run through local server via heroku local (PORT 5000)

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 express()
+
     .use(express.static(path.join(__dirname, '/app/public/css')))
-    /**
+    .use(express.static(path.join(__dirname, '/app/public/js')))
+    /*
     .set('view engine', 'ejs')
-    .get('/', (req, res)=> res.render('app/public/index'))*/
+    .get('/', (req, res)=> res.render('app/public/index'))
+    .use(express.static('/app/public'))*/
     .get('/', (req, res)=> res.sendFile(path.join(__dirname + '/app/public/index.html')))
     .listen(PORT, ()=> console.log(`Listening on PORT ${PORT}.`));
